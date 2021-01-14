482f3b8ffce1ab25e407f72359396910f9a1f103
Lazar Dudic <lazar.dudic@yahoo.com>
2021-01-14 11:27:22 +0100

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.admin', ['title' => 'Dashboard'])
+
+@section('content')
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-center">Welcome to Personal Work Log</h1>
+@endsection
+
+
+
+
