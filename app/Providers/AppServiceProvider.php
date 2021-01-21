f0d0f876dfe86cac8da8403b39ddd9ebbd7965c3
Lazar Dudic <lazar.dudic@yahoo.com>
2021-01-21 18:57:04 +0100

    Add global $current_user_id for auth user

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Job;
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::share('current_job_id', Job::withoutGlobalScope('user')->where('current_job', 1)->first()->id);
+        view()->composer('*', function($view){
+            if (\auth()->user()) {
+                $currentUserId = optional(auth()->user()->jobs()->where('current_job', 1)->first())->id;
+                $view->with('current_job_id', $currentUserId);
+            }
+        });
+
     }
 }
