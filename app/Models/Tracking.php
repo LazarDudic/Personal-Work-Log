5ce74ba67984ad08e7773673ddbe5e2edd7f26f3
Lazar Dudic <lazar.dudic@yahoo.com>
2021-01-22 14:13:34 +0100

    Add tracking mutators

--- app/Models/Tracking.php
+++ app/Models/Tracking.php
@@ -13,4 +13,33 @@ class Tracking extends Model
     protected $guarded = ['id'];
     public $timestamps = false;
 
+
+    public function setWageAttribute($wage)
+    {
+        $this->attributes['wage'] = $wage ? 1 : 0;
+    }
+
+    public function setOvertimeAttribute($overtime)
+    {
+        $this->attributes['overtime'] = $overtime ? 1 : 0;
+    }
+
+    public function setShiftDifferentialAttribute($shiftDifferential)
+    {
+        $this->attributes['shift_differential'] = $shiftDifferential ? 1 : 0;
+    }
+
+    public function setTipsAttribute($tips)
+    {
+        $this->attributes['tips'] = $tips ? 1 : 0;
+    }
+
+    public function setBonusesAttribute($bonuses)
+    {
+        $this->attributes['bonuses'] = $bonuses ? 1 : 0;
+    }    public function setExpensesAttribute($expenses)
+    {
+        $this->attributes['expenses'] = $expenses ? 1 : 0;
+    }
+
 }
